df0a4ddf33ffb809eea3c9fe652ab86638b01d0b
Raihan <52344545+Staffelin@users.noreply.github.com>
2026-01-05 17:31:50 +0700

    Push for deploy

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Replace these with your actual keys from Supabase Dashboard -> Settings -> API
-const supabaseUrl = 'https://uwktsngnovzetebafakb.supabase.co'
-const supabaseKey = 'sb_publishable_z6JpNVLeWiAnsvNX71JavA_LccaDIIn'
+// Access environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
